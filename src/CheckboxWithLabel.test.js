95d2a1c5ddc9beb41fc6197ce747e69bed43be51
Ken Duigraha Putra <kenduigraha@yahoo.com>
2016-11-28 14:14:04 +0700

    testing CheckboxWithLabel - done

--- src/CheckboxWithLabel.test.js
+++ src/CheckboxWithLabel.test.js
@@ -1,2 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
+import CheckboxWithLabel from './CheckboxWithLabel'
+
+const checkbox = TestUtils.renderIntoDocument(
+  <CheckboxWithLabel labelOn="on" labelOff="off" />
+)
+const checkboxnode = ReactDOM.findDOMNode(checkbox)
+
+it('CheckboxWithLabel changes the text before click', () => {
+
+  expect(checkboxnode.textContent).toEqual("off")
+
+})
+
+it('Clicked should be changed to on', () => {
+  TestUtils.Simulate.change(
+    TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input')
+  )
+
+  expect(checkboxnode.textContent).toEqual("on")
+})
